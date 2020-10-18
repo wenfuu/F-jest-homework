ff17aff6eeb80f67d095d2577f726e98fe22c71f
Wen Fu <faith.WilliamFu@gmail.com>
2020-10-18 16:48:56 +0800

    [Wen Fu] completed homework.

--- src/async/__test__/users.test.js
+++ src/async/__test__/users.test.js
@@ -1,5 +1,11 @@
+import getUsers from "../users";
+
+jest.mock("axios");
+
 describe("users", () => {
-  test("should get users data with mock axios get", () => {
+  test("should get users data with mock axios get", async () => {
     // TODO 13: add async test with manual mock
+    const result = getUsers();
+    await expect(result).resolves.toEqual({ mock: "get" });
   });
 });
--- src/mock.class/__test__/vaccineTest.test.js
+++ src/mock.class/__test__/vaccineTest.test.js
@@ -1,27 +1,53 @@
 import VaccineTest from "../vaccineTest";
-import Recipient from "../recipient";
 import Covid19Vaccine from "../covid19Vaccine";
+import Recipient from "../recipient";
+
+const mockAcceptInjection = jest.fn();
+const mockGetHasAntibodies = jest.fn();
 
 jest.mock("../recipient", () => {
-  // mock class实现
+  return jest.fn().mockImplementation(() => {
+    return {
+      acceptInjection: mockAcceptInjection,
+      getHasAntibodies: mockGetHasAntibodies,
+    };
+  });
 });
 
 beforeEach(() => {
   // clear mock here
+  Recipient.mockClear();
+  mockGetHasAntibodies.mockClear();
+  mockAcceptInjection.mockClear();
 });
 
 describe("inject", () => {
   test("should recipient accept injection with vaccine", () => {
     // TODO 14: add test here
+    const vaccineTest = new VaccineTest();
+
+    vaccineTest.inject();
+
+    expect(mockAcceptInjection).toHaveBeenCalledWith(
+      expect.any(Covid19Vaccine)
+    );
   });
 });
 
 describe("test", () => {
   test("should get Success if recipient has antibodies", () => {
     // TODO 15: add test here
+    mockGetHasAntibodies.mockReturnValue(true);
+    const vaccineTest = new VaccineTest();
+    const result = vaccineTest.test();
+    expect(result).toBe("Vaccine Test Success");
   });
 
   test("should get Failed if recipient has no antibodies", () => {
     // TODO 16: add test here
+    mockGetHasAntibodies.mockReturnValue(false);
+    const vaccineTest = new VaccineTest();
+    const result = vaccineTest.test();
+    expect(result).toBe("Vaccine Test Failed");
   });
 });
